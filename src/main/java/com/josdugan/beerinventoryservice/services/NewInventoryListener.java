b317453629df7f164c13513ad3943687c648bece
josdugan <josdugan@gmail.com>
2022-06-16 19:31:34 -0400

    Add NewInventoryListener and update maven parent

---
+++ src/main/java/com/josdugan/beerinventoryservice/services/NewInventoryListener.java
@@ -0,0 +1,32 @@
+package com.josdugan.beerinventoryservice.services;
+
+import com.josdugan.beerinventoryservice.config.JmsConfig;
+import com.josdugan.beerinventoryservice.domain.BeerInventory;
+import com.josdugan.beerinventoryservice.repositories.BeerInventoryRepository;
+import com.josdugan.beerworkscommon.dtos.BeerDto;
+import com.josdugan.beerworkscommon.events.NewInventoryEvent;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.jms.annotation.JmsListener;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@RequiredArgsConstructor
+@Component
+public class NewInventoryListener {
+
+    private final BeerInventoryRepository beerInventoryRepository;
+
+    @JmsListener(destination = JmsConfig.NEW_INVENTORY_QUEUE)
+    public void listen(NewInventoryEvent event) {
+        log.debug("Got Inventory: " + event.toString());
+
+        BeerDto beerDto = event.getBeerDto();
+
+        beerInventoryRepository.save(BeerInventory.builder()
+                .beerId(beerDto.getId())
+                .upc(beerDto.getUpc())
+                .quantityOnHand(beerDto.getQuantityOnHand())
+                .build());
+    }
+}
